004a46b38fdc9eecbf88382a4f472dec40d06745
“derasann” <kyonodera@gmail.com>
2024-04-16 19:24:11 +0900

    twentith commit

--- toml.py
+++ toml.py
@@ -3,41 +3,34 @@ from PIL import Image
 import os
 import toml
 
-
-# ユーザー名とパスワードの入力
-username = st.text_input("Username")
-password = st.text_input("Password", type="password")
-
 # TOMLファイルからパスワード等の情報を読み込む
 current_dir = os.path.dirname(os.path.abspath(__file__))
 toml_file_path = os.path.join(current_dir, "main.toml")
 config = toml.load(toml_file_path)
 
-# デバッグ用にconfig変数の内容を確認する
-print(config)
+# パスワードの入力
+entered_password = st.text_input("Password", type="password")
 
 # ユーザー入力と比較してアクセス制限をかける
-if username == config["database"]["user"] and password == config["auth"]["password"]:
+auth_token = st.secrets["auth_token"]
+if entered_password.strip() == auth_token:
+    # 認証成功時の処理
     st.title("Welcome to the app!")
+    
+    # ここから好きな映画の表示
+    st.title('好きな映画')
+    st.write('Display Image')
+    
+    # 相対パスの例
+    img_path = "topgun.jpg"
+    img = Image.open(img_path)
+    st.image(img, caption='マーベリック！', use_column_width=False, width=300)
+    
+    option = st.slider(
+        'この映画をどのくらい好きですか、',
+        min_value=1, max_value=10, value=5
+    )
+    st.write(f'あなたの評価は、{option} です。')
 else:
-    st.error("Invalid username or password")
-    st.stop()
-
-# カレントディレクトリを取得して表示（デバッグ用）
-current_dir = os.getcwd()
-#st.write('Current directory:', current_dir)
-
-# ここから前のデータ
-st.title('好きな映画')
-st.write('Display Image')
-
-# 相対パスの例
-img_path = "topgun.jpg"
-img = Image.open(img_path)
-st.image(img, caption='マーベリック！', use_column_width=False, width=300)
-
-option = st.slider(
-    'この映画をどのくらい好きですか、',
-    min_value=1, max_value=10, value=5
-)
-'あなたの評価は、', option, 'です。'
+    if entered_password:
+        st.error("Invalid password")
